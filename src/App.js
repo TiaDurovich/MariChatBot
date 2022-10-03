6f6a87c0f72fa662e9ad82e2620fdc3e63cdc60a
Tia Durovich <tia.durovich@hotmail.com>
2022-10-03 16:43:37 +1300

    Config, MessageParser & ActionProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React from 'react';
 import { Chatbot } from 'react-chatbot-kit'
+
+import MessageParser from './MessageParser';
+import ActionProvider from './ActionProvider';
+import config from './config';
+
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot config={config} MessageParser={MessageParser} ActionProvider={ActionProvider}/>
       </header>
     </div>
   );
