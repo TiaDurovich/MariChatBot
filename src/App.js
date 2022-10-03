6bfcb4795a6b1816f4df0046fb24200e01a63a5e
Tia Durovich <tia.durovich@hotmail.com>
2022-10-03 16:32:32 +1300

    Initiated Sass and SourceMaps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Chatbot } from 'react-chatbot-kit'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World
+        <Chatbot />
       </header>
     </div>
   );
