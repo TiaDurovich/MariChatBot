4e8432fa97056ddf4e92237620cee045e8b6da4a
Tia Durovich <tia.durovich@hotmail.com>
2022-10-04 14:29:46 +1300

    Chatbot Initiated

---
+++ src/ActionProvider.js
@@ -0,0 +1,17 @@
+// ActionProvider starter code
+
+import React from 'react';
+
+const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {},
+        });
+      })}
+    </div>
+  );
+};
+
+export default ActionProvider;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Chatbot } from 'react-chatbot-kit'
+import Chatbot from 'react-chatbot-kit'
 import 'react-chatbot-kit/build/main.css'
+import './App.css';
 
 import config from './config.js';
-import MessageParser from './MessageParser.jsx';
-import ActionProvider from './ActionProvider.jsx';
-
-import './App.css';
+import MessageParser from './MessageParser.js';
+import ActionProvider from './ActionProvider.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot config={config} MessageParser={MessageParser} ActionProvider={ActionProvider}/>
+        <Chatbot 
+        config={config} 
+        messageParser={MessageParser} 
+        actionProvider={ActionProvider} />
       </header>
     </div>
   );
