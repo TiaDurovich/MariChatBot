d82dc6688228127c69d1834db4cb286980d04b50
Tia Durovich <tia.durovich@hotmail.com>
2022-10-06 10:46:53 +1300

    Option buttons esablished on screen

--- src/Options.js
+++ src/Options.js
@@ -12,4 +12,14 @@ const Options = (props) => {
         {text: "Option 2", handler: () => {},id: 2},
         {text: "Option 3", handler: () => {},id: 3},
     ];
-}
+
+    const buttonsMarkup = options.map((option) => (
+        <button key={option.id} onclick={option.handler} className="option-button">
+            {option.text}
+        </button>
+    ));
+
+    return <div className="options-container">{buttonsMarkup}</div>
+};
+
+export default Options;
--- src/config.js
+++ src/config.js
@@ -1,11 +1,20 @@
-// Config starter code
+import React from "react";
 import { createChatBotMessage } from "react-chatbot-kit";
+import Options from "../src/Options";
 
 const botName = 'the Marist College NCEA Chatbot'
 
 const config = {
   initialMessages: [
-    createChatBotMessage(`Hello, I am here to help with your NCEA. What year level are you in?`),
+    createChatBotMessage(`Hello, I am here to help with your NCEA.`, {
+      widget: "options",
+    }),
+  ],
+  widgets: [
+    {
+      widgetName: "options",
+      widgetFunc: (props) => <Options {...props} />,
+    },
   ],
   botName: botName,
   
