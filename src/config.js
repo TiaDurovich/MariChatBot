ebb744ccab50d0f04b20f56be6645be64a86dea0
Tia Durovich <tia.durovich@hotmail.com>
2022-10-12 18:07:44 +1300

    Alert onload

--- src/config.js
+++ src/config.js
@@ -4,6 +4,11 @@ import Options from "../src/Options.js";
 
 const botName = 'the Marist College NCEA Chatbot'
 
+window.onload = function onload() {
+  alert("Welcome to the Marist College NCEA Chatbot. Please note that the chatbot is spelling sensitive.") 
+}
+
+
 const config = {
   initialMessages: [
     createChatBotMessage(`Hello, I am here to help with your NCEA. Below are some topics to get you started.`, {
